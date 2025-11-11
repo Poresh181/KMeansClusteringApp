9287c675add983745d290577a300515b72545263
Poresh181 <poresh450@gmail.com>
2025-11-12 00:40:50 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -24,5 +24,6 @@ else:
   uploaded_file = st.sidebar.file_uploader("Upload Your CSV File", type = ['csv'])
   if uploaded_file:
     df = pd.read_csv(uploaded_file)
+  else:
     st.warning("Please upload your csv file or use the sample dataset")
     st.stop()
