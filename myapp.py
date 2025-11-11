4ac30988a317348003e9ef8e827ff55be5d28abf
Poresh181 <poresh450@gmail.com>
2025-11-12 00:12:12 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -8,3 +8,8 @@ from sklearn.cluster import KMeans
 
 st.title("KMeans Clustering App")
 st.subheader("Data Science With Poresh")
+
+#Sidebar
+
+st.sidebar.header("Upload CSV Data or Use Sample")
+st.sidebar.checkbox("Use Example Dataset")
