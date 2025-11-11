06b8da7161ab9fa4fdd24e1047f71bbd63b03940
Poresh181 <poresh450@gmail.com>
2025-11-11 22:18:03 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
+
+st.title("KMeans Clustering App")
+st.subheader("Data Science With Poresh")
